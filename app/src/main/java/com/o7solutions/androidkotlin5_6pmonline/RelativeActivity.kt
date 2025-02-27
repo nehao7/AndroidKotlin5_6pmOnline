cc19a916c646b57fba72909860bed7566c78c823
Neha <neha@o7tec.com>
2025-02-27 17:53:59 +0530

    Button click, Intent , Toast , harcoded text

--- app/src/main/java/com/o7solutions/androidkotlin5_6pmonline/RelativeActivity.kt
+++ app/src/main/java/com/o7solutions/androidkotlin5_6pmonline/RelativeActivity.kt
@@ -1,32 +1,20 @@
 package com.o7solutions.androidkotlin5_6pmonline
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    var button:Button?=null
+class RelativeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_relative)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        button=findViewById(R.id.btnClick)
-        button?.setOnClickListener {
-            var intent=Intent(this,RelativeActivity::class.java)
-            startActivity(intent)
-            Toast.makeText(this,"My button is clicked",Toast.LENGTH_SHORT).show()
-        }
-
     }
 }
