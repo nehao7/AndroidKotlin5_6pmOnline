197b670447c4e96d0297a44247765d8b4ea69e63
nehao7 <157192328+nehao7@users.noreply.github.com>
2025-06-14 17:00:34 +0530

    direct api integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,11 @@ dependencies {
 
     //razorpay
     implementation ("com.razorpay:checkout:1.6.39")
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
 
 //    fused location client
     implementation("com.google.android.gms:play-services-location:21.3.0")
